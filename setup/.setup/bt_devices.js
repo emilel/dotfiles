65629077e738b50435ded901e4230bd323d72d07
Emil Eliasson <limeliasson@gmail.com>
2021-09-22 19:08:26 +0200

    hard work work

--- setup/.setup/bt_devices.js
+++ setup/.setup/bt_devices.js
@@ -1,5 +1,7 @@
 {
-  "harman": "00:1D:DF:7B:AD:E3",
-  "sonny": "94:DB:56:52:B5:87",
-  "kalle": "F8:FA:52:B1:DE:5C"
+  "har": "00:1D:DF:7B:AD:E3",
+  "son": "94:DB:56:52:B5:87",
+  "kal": "F8:FA:52:B1:DE:5C",
+  "log": "EA:19:20:8E:6A:69",
+  "lin": "F8:FA:52:B1:DE:5C"
 }
