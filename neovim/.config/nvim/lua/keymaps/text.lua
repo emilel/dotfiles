6468bb7432683e00a36d0f2fffcebd235ec5ab9e
Emil Eliasson <limeliasson@gmail.com>
2024-08-15 15:02:42 +0200

    smol

--- neovim/.config/nvim/lua/keymaps/text.lua
+++ neovim/.config/nvim/lua/keymaps/text.lua
@@ -5,3 +5,6 @@ vim.keymap.set('v', '<c-k>', ':m \'<-2<cr>gv=gv', { desc = 'Move line up' })
 -- add lines
 vim.keymap.set('n', '<space>o', 'mzo<esc>`z', { desc = 'Add line below' })
 vim.keymap.set('n', '<space>O', 'mzO<esc>`z', { desc = 'Add line above' })
+
+-- keep cursor still after visual mode
+vim.keymap.set('x', 'y', 'ygv<esc>', { desc = 'Keep cursor when copying visual selection' })
---
+++ setup/README.md
@@ -0,0 +1,1 @@
+* to install sway on ubuntu with nvidia, add `--unsupported-gpu` to the sway command in `/usr/share/wayland-sessions/sway.desktop`
