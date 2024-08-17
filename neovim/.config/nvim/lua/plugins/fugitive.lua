b689672ca63b573a6cf3d3ede8227db9ee4d80b1
Emil Eliasson <limeliasson@gmail.com>
2024-08-17 17:14:11 +0200

    change git mappings

--- neovim/.config/nvim/lua/plugins/fugitive.lua
+++ neovim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,7 +2,9 @@ return {
     'tpope/vim-fugitive',
     lazy = true,
     keys = {
-        { '<space>g', '<cmd>Git<cr><cmd>only<cr>', { desc = 'Open git' } },
-        { '<space>G', ':Git ', { desc = 'Run git command' } }
+        { '<space>gg', '<cmd>Git<cr><cmd>only<cr>', { desc = 'Open git' } },
+        { '<space>gj', '<cmd>Git pull<cr>', { desc = 'Git pull' } },
+        { '<space>gk', '<cmd>Git push<cr>', { desc = 'Git push' } },
+        { '<space>gc', ':Git ', { desc = 'Run git command' } }
     }
 }
