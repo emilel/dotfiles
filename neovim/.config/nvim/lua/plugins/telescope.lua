fd9323b8780931b0e1dfda91b0bbb150cc05f65e
Emil Eliasson <limeliasson@gmail.com>
2024-08-19 18:28:51 +0200

    break out function

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,29 @@
+local function go_to_directory()
+  local actions = require('telescope.actions')
+  local action_state = require('telescope.actions.state')
+  local pickers = require('telescope.pickers')
+  local finders = require('telescope.finders')
+  local conf = require('telescope.config').values
+
+  pickers.new({}, {
+    prompt_title = "Find Folders",
+    finder = finders.new_oneshot_job({ "fd", "--type", "d", "--hidden", "--exclude", ".git" }, {}),
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        local selection = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        vim.cmd('NnnPicker ' .. vim.fn.fnameescape(selection[1]))
+        vim.defer_fn(function()
+          vim.cmd('startinsert')
+        end, 100)
+      end)
+      return true
+    end,
+  }):find()
+end
+
+
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
@@ -43,33 +69,10 @@ return {
       'Show mappings'
     },
     {
-      '<space><c-g>',
-      function()
-        local actions = require('telescope.actions')
-        local action_state = require('telescope.actions.state')
-        local pickers = require('telescope.pickers')
-        local finders = require('telescope.finders')
-        local conf = require('telescope.config').values
-
-        pickers.new({}, {
-          prompt_title = "Find Folders",
-          finder = finders.new_oneshot_job({ "fd", "--type", "d", "--hidden", "--exclude", ".git" }, {}),
-          sorter = conf.generic_sorter({}),
-          attach_mappings = function(prompt_bufnr)
-            actions.select_default:replace(function()
-              local selection = action_state.get_selected_entry()
-              actions.close(prompt_bufnr)
-              vim.cmd('NnnPicker ' .. vim.fn.fnameescape(selection[1]))
-              vim.defer_fn(function()
-                vim.cmd('startinsert')
-              end, 100)
-            end)
-            return true
-          end,
-        }):find()
-      end,
+      '<c-g>',
+      go_to_directory,
       desc = 'Go to directory'
-    }
+    },
   },
   config = function()
     require('telescope').setup({
