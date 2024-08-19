3cbe8119ec7c0be7eb5f9382c395f29bad630d8d
Emil Eliasson <limeliasson@gmail.com>
2024-08-19 14:49:38 +0200

    find file

--- neovim/.config/nvim/lua/keymaps/autocompletion.lua
+++ neovim/.config/nvim/lua/keymaps/autocompletion.lua
@@ -1,9 +1,11 @@
 -- trigger autocompletion menu
 vim.keymap.set('i', '<c-l>', function()
-    if vim.fn.pumvisible() == 1 then
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<c-y>', true, true, true), 'n', true)
-    else
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<c-x><c-o>', true, true, true), 'n', true)
-    end
+  if vim.fn.pumvisible() == 1 then
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<c-y>', true, true, true), 'n', true)
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<c-x><c-o>', true, true, true), 'n', true)
+  end
 end
 , { desc = 'Trigger autocompletion' })
+
+vim.keymap.set('i', '<c-f>', '<c-x><c-f>', { desc = 'Autocomplete file' })
