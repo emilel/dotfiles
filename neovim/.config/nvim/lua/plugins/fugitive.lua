efc6bf141af80950dba28abdaab57c92d84de8d0
Emil Eliasson <limeliasson@gmail.com>
2024-08-27 15:52:19 +0200

    change mappings

--- neovim/.config/nvim/lua/plugins/fugitive.lua
+++ neovim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
     'tpope/vim-fugitive',
     lazy = true,
     keys = {
-        { '<c-g>g', '<cmd>Git<cr><cmd>only<cr>', { desc = 'Open git' } },
+        { '<c-g><c-g>', '<cmd>Git<cr><cmd>only<cr>', { desc = 'Open git' } },
         { '<c-g>j', '<cmd>Git pull<cr>', { desc = 'Git pull' } },
         { '<c-g>k', '<cmd>Git push<cr>', { desc = 'Git push' } },
         { '<c-g>c', ':Git ', { desc = 'Run git command' } }
--- neovim/.config/nvim/lua/plugins/gp.lua
+++ neovim/.config/nvim/lua/plugins/gp.lua
@@ -2,32 +2,27 @@ return {
   'Robitx/gp.nvim',
   keys = {
     {
-      '<space><space><c-s>',
+      '<c-s>n',
       '<cmd>GpChatNew popup<cr>',
       desc = 'Start new AI chat'
     },
     {
-      '<c-s>',
+      '<c-s><space>',
       ':GpChatPaste popup<cr>',
       mode = 'x',
       desc = 'Paste to AI chat',
     },
     {
-      '<space><c-s>',
+      '<c-s>r',
       '<cmd>GpRewrite<cr>',
       mode = 'x',
       desc = 'Rewrite selected text with AI'
     },
     {
-      '<c-s>',
+      '<c-s><space>',
       '<cmd>GpChatToggle popup<cr>',
       desc = 'Toggle AI chat'
     }
-  },
-  cmd = {
-    'GpChatNew',
-    'GpChatPaste',
-    'GpChatToggle',
   },
   config = function()
     require('gp').setup({
