37af6c75e33552d8b404f696d5477e81123fca45
Emil Eliasson <limeliasson@gmail.com>
2024-09-13 22:12:52 +0200

    didn't seem necessary

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,13 +1,6 @@
 return {
   'lewis6991/gitsigns.nvim',
   config = function()
-    -- vim.api.nvim_create_autocmd("User", {
-    --   pattern = "GitCommit",
-    --   callback = function()
-    --     require('gitsigns').refresh()
-    --   end
-    -- })
-
     require('gitsigns').setup({})
   end
 }
