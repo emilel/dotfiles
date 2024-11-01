b5610319899162209eb6f69385d24f2caa396180
Emil Eliasson <emil.eliasson@ellipticlabs.com>
2024-11-01 10:08:57 +0100

    edit stdin and send to stdout

--- neovim/.config/nvim/lua/commands.lua
+++ neovim/.config/nvim/lua/commands.lua
@@ -5,6 +5,17 @@ vim.api.nvim_create_user_command('TempFile', function()
     yank.set_exit_keymap('q!')
 end, { nargs = '*' })
 
+vim.api.nvim_create_user_command('Pipe', function(opts)
+    local path = opts.args
+
+    if vim.loop.fs_stat(path) then
+        vim.cmd('edit ' .. vim.fn.fnameescape(path))
+        vim.keymap.set('n', '<cr><cr>', '<cmd>wq<cr>', { buffer = true, desc = 'Write to stdout' })
+    else
+        print('Error: Path does not exist: ' .. path)
+    end
+end, { nargs = 1 })
+
 vim.api.nvim_create_user_command('Temp', function()
     yank.open_buffer()
     yank.set_exit_keymap('bd!')
--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -94,3 +94,11 @@ catdir() {
       echo
   done
 }
+
+edit() {
+    tmpfile=$(mktemp)
+    trap 'rm -f "$tmpfile"' EXIT
+    cat - > "$tmpfile"
+    nvim +"Pipe $tmpfile" < /dev/tty > /dev/tty
+    cat "$tmpfile"
+}
