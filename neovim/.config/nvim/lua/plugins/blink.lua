5d3febc4a745919ef1505714e02651ec3a51dc73
Emil Eliasson <limeliasson@gmail.com>
2025-10-04 16:29:06 +0200

    no need for autocomplete then

--- neovim/.config/nvim/lua/plugins/blink.lua
+++ neovim/.config/nvim/lua/plugins/blink.lua
@@ -1,7 +1,6 @@
 return {
     'saghen/blink.cmp',
     dependencies = { 'rafamadriz/friendly-snippets' },
-
     version = '1.*',
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
@@ -17,7 +16,7 @@ return {
             ['<c-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
         },
         signature = { enabled = true, window = { show_documentation = true } },
-
+        enabled = function() return not vim.tbl_contains({ "markdown", "gitcommit" }, vim.bo.filetype) end,
         appearance = {
             nerd_font_variant = 'mono',
             kind_icons = {
