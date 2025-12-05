f4c547acbdbec947d4733dd44dc8b26cdee9034c
Emil Eliasson <limeliasson@gmail.com>
2025-12-05 21:52:03 +0100

    ncie

--- neovim/.config/nvim/after/ftplugin/python.lua
+++ neovim/.config/nvim/after/ftplugin/python.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "\\st", 'o__import__("ipdb").set_trace()<esc>', { buffer = true, desc = "Set trace" })
 vim.keymap.set("n", "\\l", 'my<cmd>g/__import__("ipdb").set_trace()/d<cr>\'y', { buffer = true, desc = "Clear trace" })
 vim.keymap.set("n", "\\e", 'o__import__("sys").exit(-1)<esc>', { buffer = true, desc = "Exit" })
+vim.keymap.set("x", "\\a", '"yy<esc>oprint(f"")<esc>h"yPa: {}<esc>"yPV', { buffer = true, desc = "Exit" })
